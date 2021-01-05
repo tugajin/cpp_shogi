c05fe1a8f31a8e75ce74373951dfb5c4373d694d
tugajin <mamorukai2009@gmail.com>
2021-01-06 01:04:01 +0900

    speed up

--- src/lib/libmain.cpp
+++ src/lib/libmain.cpp
@@ -5,26 +5,26 @@
 
 #include <boost/python/numpy.hpp>
 
-namespace p = boost::python;
+namespace py = boost::python;
 namespace np = boost::python::numpy;
 
 TeeStream Tee;
 
-void lib_sfen_to_tensor(boost::python::list &sfen_list, boost::python::numpy::ndarray &numpy_feat) {
-    const auto len = boost::python::len(sfen_list);
+void lib_sfen_to_tensor(py::list &sfen_list, np::ndarray &numpy_feat) {
+    const auto len = py::len(sfen_list);
+    auto feat = new float[len][POS_END_SIZE][FILE_SIZE][RANK_SIZE]();
     for(auto batch_index = 0; batch_index < len; batch_index++) {
-        std::string sfen = boost::python::extract<std::string>(sfen_list[batch_index]);
+        std::string sfen = py::extract<std::string>(sfen_list[batch_index]);
         Pos pos = pos_from_sfen(sfen);
-        float feat[POS_END_SIZE][FILE_SIZE][RANK_SIZE] = {};
-        make_feat(pos, feat);
-        for(auto feat_index = 0; feat_index < POS_END_SIZE; feat_index++) {
-            for(auto file = 0; file < FILE_SIZE; ++file) {
-                for(auto rank = 0; rank < RANK_SIZE; ++rank) {
-                    numpy_feat[batch_index][feat_index][file][rank] = feat[feat_index][file][rank];
-                }
-            }
-        }
+        make_feat(pos, &feat[batch_index][0]);
     }
+    py::tuple shape = py::make_tuple(len, int(POS_END_SIZE),int(FILE_SIZE),int(RANK_SIZE));
+    py::tuple stride = py::make_tuple(sizeof(float)*int(POS_END_SIZE*FILE_SIZE*RANK_SIZE),sizeof(float)*int(FILE_SIZE*RANK_SIZE),sizeof(float)*int(RANK_SIZE),sizeof(float));
+    np::dtype dt = np::dtype::get_builtin<float>();
+    np::ndarray np_feat = np::from_data(feat, dt, shape, stride, py::object());
+    numpy_feat = np_feat.copy();
+    
+    delete[] feat;
 }
 
 int lib_move_to_index(std::string sfen_move, std::string sfen_turn) {
@@ -54,8 +54,8 @@ BOOST_PYTHON_MODULE(cpp_lib) {
 
     Py_Initialize();
     np::initialize();
-    p::def("sfen_to_tensor", lib_sfen_to_tensor);
-    p::def("move_to_index", lib_move_to_index);
+    py::def("sfen_to_tensor", lib_sfen_to_tensor);
+    py::def("move_to_index", lib_move_to_index);
 
 }
 
